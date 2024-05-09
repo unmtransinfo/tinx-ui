fc8ec6e7738d45bcd38213024b866d86e5ae3472
Jon Wahl <jwahl@elevatodigital.com>
2024-05-09 18:00:35 -0500

    API_ROOT not sticking.

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 const config = {};
 
+console.log('API_ROOT', process.env.API_ROOT);
+
 config.development = {
   //API_ROOT: 'localhost:8000'
   API_ROOT: process.env.API_ROOT ? process.env.API_ROOT : 'https://api-staging.newdrugtargets.org'
