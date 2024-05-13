4630c8b59c4268777fb2b0c79061deebe177face
Jon Wahl <jwahl@elevatodigital.com>
2024-05-13 14:03:34 -0500

    hard code config url

--- src/config.js
+++ src/config.js
@@ -1,18 +1,16 @@
 const config = {};
 
-console.log('API_ROOT', process.env.API_ROOT);
-
 config.development = {
   //API_ROOT: 'localhost:8000'
-  API_ROOT: process.env.API_ROOT ? process.env.API_ROOT : 'https://api-staging.newdrugtargets.org'
+  API_ROOT: 'https://api-staging.newdrugtargets.org'
 };
 
 config.staging = {
-  API_ROOT: process.env.API_ROOT ? process.env.API_ROOT : 'https://chiltepin.health.unm.edu/tinx'
+  API_ROOT: 'https://chiltepin.health.unm.edu/tinx'
 };
 
 config.production = {
-  API_ROOT: process.env.API_ROOT ? process.env.API_ROOT : 'https://api.newdrugtargets.org'
+  API_ROOT: 'https://api.newdrugtargets.org'
 };
 
 export default config[process.env.NODE_ENV];
